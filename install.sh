69839433b2d11d7121258f31fa7f82b473f998a5
Konstantinos Despoinidis <kdesp2003@gmail.com>
2024-02-01 02:50:53 +0200

    installing man page

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ executable_path=$(find "$build_folder" -maxdepth 1 -type f -executable | head -n
 as="cchat"
 
 if [ -n "$executable_path" ]; then
+    echo "Installing..."
     executable_name=$(basename "$executable_path")
     
     sudo cp "$executable_path" /usr/bin/$as
@@ -21,8 +22,16 @@ if [ -n "$executable_path" ]; then
         echo "Error: Failed to copy the executable to /usr/bin/."
     fi
 else
+    echo "Building..."
     make cchat
 	if [ $? -eq 0 ]; then
         ./install.sh
 	fi
 fi
+
+echo "Installing man page..."
+
+install -g 0 -o 0 -m 0644 cchat.1 /usr/share/man/man8/
+gzip /usr/share/man/man8/cchat.1
+
+echo "man page installed successfully"
