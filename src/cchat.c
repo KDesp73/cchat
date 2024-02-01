13bc84e1dc54c9969867f46160aba152163d70e8
Konstantinos Despoinidis <kdesp2003@gmail.com>
2024-02-01 03:11:00 +0200

    validating ip address and port

--- src/cchat.c
+++ src/cchat.c
@@ -184,9 +184,16 @@ void print_args(int argc, char** argv){
     printf("\n");
 }
 
+int isValidIpAddress(char *ipAddress){
+    struct sockaddr_in sa;
+    int result = inet_pton(AF_INET, ipAddress, &(sa.sin_addr));
+    return result != 0;
+}
+
+
 int main(int argc, char** argv){
-    int port;
-    char* ip_address;
+    int port = -345678;
+    char* ip_address = NULL;
 
     struct option long_options[] = {
         {"address", required_argument, NULL, 'a'},
@@ -208,6 +215,29 @@ int main(int argc, char** argv){
                 exit(1);
         }
     }
+
+    if(port ==  -345678){
+        ERRO("No port specified\n");
+        exit(1);
+    } 
+
+    if(ip_address == NULL){
+        ERRO("No ip address specified\n");
+        exit(1);
+    }
+
+    if(!isValidIpAddress(ip_address)){
+        ERRO("Invalid ip address\n");
+        exit(1);
+    }
+
+    // Port should range between 1024 and 49151
+    if(port < 1024 || port > 49151) {
+        ERRO("Port should range between 1024 and 49151\n");
+        exit(1);
+    }
+
+
 
     char* command = argv[argc-1];
 
